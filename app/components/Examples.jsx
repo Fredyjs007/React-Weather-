dd62f919139c4e5a90a866aba60028f21b9586c2
Fredy Rosales <fredyr78@yahoo.com>
2016-11-17 12:43:27 -0800

    Rename Example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@
   var Examples = (props) => {
     return (
       <div>
-        <h3>Examples Component!</h3>
+        <h3>Examples</h3>
         <P>Welcom to examples page!</P>
       </div>
     )
